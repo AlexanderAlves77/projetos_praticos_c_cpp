b40284578fff6801ef9fd2ecd48f534ede64848a
Alexander Alves <vendas77.rj@gmail.com>
2023-06-10 12:26:03 -0300

    Terminando o projeto WinDXUT

---
+++ exercicios_cpp_computacao_grafica/Aula_02/WinDXUT/WinDXUT/DXUT.h
@@ -0,0 +1,19 @@
+/**********************************************************************************
+// DXUT (Arquivo de Cabeçalho)
+//
+// Criação:     09 Jun 2023
+// Atualização: 09 Jun 2023
+// Compilador:  Visual C++ 2019
+//
+// Descrição:   Arquivo mestre para a biblioteca DirectX Utility (DXUT)
+//
+**********************************************************************************/
+
+#ifndef DXUT_DirectX_Utility_H
+#define DXUT_DirectX_Utility_H
+
+#include "Types.h"
+#include "Resources.h"
+#include "Window.h"
+
+#endif
