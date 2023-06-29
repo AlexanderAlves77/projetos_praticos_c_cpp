d414a4164548877bed0d81f032d543cc09de3b5a
Alexander Alves <vendas77.rj@gmail.com>
2023-06-29 07:57:29 -0300

    Exercicio Lista

---
+++ exercicios_c_cpp_completo/Aula_02/PrimeiroListaEx01/PrimeiraListaEx01.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main()
+{
+	std::cout 
+		<< "Vou" << "\n"
+		<< "Aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem" << "\n"
+		<< "C++" << "\n";
+	system("PAUSE");
+	return 0;
+}
