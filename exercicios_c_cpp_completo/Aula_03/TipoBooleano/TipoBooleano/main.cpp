03ff63e4f27ef215d955fb157c1fe956595d1099
Alexander Alves <vendas77.rj@gmail.com>
2023-06-29 08:28:16 -0300

    Corrigindo Tipo Booleano

--- exercicios_c_cpp_completo/Aula_03/TipoBooleano/TipoBooleano/main.cpp
+++ exercicios_c_cpp_completo/Aula_03/TipoBooleano/TipoBooleano/main.cpp
@@ -2,5 +2,15 @@
 
 int main(int argc, char* argv[])
 {
+	bool buzinar = false;
 
+	std::cout << "Buzinar? ";
+	std::cin >> buzinar;
+
+	if (buzinar == true)
+		std::cout << "Buzina\a\a\a\a\a\n";
+	else
+		std::cout << "Silêncio" << std::endl;
+
+	return 0;
 }
