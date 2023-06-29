28fe9bd9059de456177a8c338215aee8b1620c3c
Alexander Alves <vendas77.rj@gmail.com>
2023-06-29 07:56:13 -0300

    Primeira lista

---
+++ exercicios_c_cpp_completo/Aula_02/PrimeiraListaEx02/PrimeiraListaEx02.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+int main()
+{
+	// Primeiro a corde fundo e depois a cor da letra
+	system("COLOR 47");
+	std::cout
+		<< "Vou" << "\n"
+		<< "Aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem" << "\n"
+		<< "C++" << "\n";
+	system("PAUSE");
+	return 0;
+}
