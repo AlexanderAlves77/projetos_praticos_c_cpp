5b1345c3296143dce26b056bce2ccf5d2e511a16
Alexander Alves <vendas77.rj@gmail.com>
2023-08-23 12:07:02 -0300

    Finally overview with class

--- 03_Learn_C++_Programming_Making_Games/Section02_Get_to_class/ArcadeApp/ArcadeApp/main.cpp
+++ 03_Learn_C++_Programming_Making_Games/Section02_Get_to_class/ArcadeApp/ArcadeApp/main.cpp
@@ -10,10 +10,36 @@
 #include <iostream>
 using namespace std;
 
+class Point2D
+{
+private:
+	int mX;
+	int mY;
+	void MyPrivateMethod();
+
+public:
+	void SetX(int x);
+	void SetY(int y);
+	int GetX();
+	int GetY();
+	void Display();
+};
+
+void Point2D::SetX(int x) { mX = x; }
+void Point2D::SetY(int y) { mY = y; }
+int Point2D::GetX() { return mX; }
+int Point2D::GetY() { return mY; }
+
+void Point2D::Display()
+{
+	cout << "X: " << mX << ", Y: " << mY << endl;
+}
+
 int main(int argc, const char argv[])
 {
-	cout << "!!!Hello Udemy!!!" << endl;
-
+	Point2D myPoint;
+	myPoint.SetX(0);
+	cout << "Size of myPoint is: " << sizeof(myPoint) << endl;
 
 	cout << endl;
 	system("Pause");
