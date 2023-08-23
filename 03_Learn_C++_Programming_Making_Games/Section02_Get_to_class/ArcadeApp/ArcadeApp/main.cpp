654796ea95ba25bd6fd19b5019fbf7e3eb990f58
Alexander Alves <vendas77.rj@gmail.com>
2023-08-23 11:38:05 -0300

    Overview Arcade App Project

---
+++ 03_Learn_C++_Programming_Making_Games/Section02_Get_to_class/ArcadeApp/ArcadeApp/main.cpp
@@ -0,0 +1,21 @@
+/**********************************************************************************************
+* Name            : ArcadeApp.cpp
+* Author          : Alexander Alves
+* Version         : 0.0.1
+* Copyright       : 
+* Description     : Hello World in C++, Ansi-style
+* 
+**********************************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char argv[])
+{
+	cout << "!!!Hello Udemy!!!" << endl;
+
+
+	cout << endl;
+	system("Pause");
+	return 0;
+}
