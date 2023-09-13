7d75eb803e8b3193ccf7a3c0f70893588f2170da
Alexander Alves <vendas77.rj@gmail.com>
2023-09-13 18:01:14 -0300

    Adding Arcade_Header h

---
+++ 03_Learn_C++_Programming_Making_Games/Section08_Lets_make_a_thing_A_line/Header/Arcade_Head.h
@@ -0,0 +1,23 @@
+/*=================================================================================
+*  Arquivo     : Arcade_Head.h
+*  Author      : Alexander Alves
+*  Criação     : 13 Set 2023
+*  Compilador  : Visual C++ 2019
+*  Atualização : 13 Set 2023
+*
+*  Descrição:   Hello World in C++, Ansi-style
+===================================================================================*/
+
+#ifndef ARCADE_HEAD_H_
+#define ARCADE_HEAD_H_
+
+#include <SDL.h>
+#include "../Graphics/Color.h"
+#include "../Graphics/Screen.h"
+#include "../Graphics/ScreenBuffer.h"
+#include "../Shapes/Line2D.h"
+#include "../Utils/Utils.h"
+#include "../Utils/Vec2D.h"
+
+
+#endif /* ARCADE_HEAD_H_ */
