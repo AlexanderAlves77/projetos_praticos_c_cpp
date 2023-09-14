58c10539dddbb5ceee830a184f4d93d1b0ae5364
Alexander Alves <vendas77.rj@gmail.com>
2023-09-14 14:16:03 -0300

    Modificando App h

--- 02_exercicios_cpp_computacao_grafica/Aula_05/DXGI_DXUT/App.h
+++ 02_exercicios_cpp_computacao_grafica/Aula_05/DXGI_DXUT/App.h
@@ -1,16 +1,14 @@
-/**********************************************************************************
-// App (Arquivo de Cabeçalho)
-// 
-// Criação:		11 Jan 2020
-// Atualização:	04 Ago 2021
-// Compilador:	Visual C++ 2019
-//
-// Descrição:	Uma classe abstrata para representar uma aplicação
-//
-//              Todas as aplicações derivadas da classe App devem
-//              sobrescrever os métodos Init, Update e Finalize. 
-//
-**********************************************************************************/
+/*=================================================================================
+*  Arquivo     : App (Código Fonte)
+*  Author      : Alexander Alves
+*  Criação     : 14 Set 2023
+*  Compilador  : Visual C++ 2019
+*  Atualização : 14 Set 2023
+*
+*  Descrição   : Uma classe abstrata para representar uma aplicação
+*                Todas as aplicações derivadas da classe App devem
+*                sobrescrever os métodos Init, Update, Draw e Finalize.
+===================================================================================*/
 
 #ifndef DXUT_APP_H
 #define DXUT_APP_H
