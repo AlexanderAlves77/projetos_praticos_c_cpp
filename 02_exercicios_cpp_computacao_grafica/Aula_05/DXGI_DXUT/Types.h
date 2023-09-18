0a453205ff504febd391adf26aecbe92463ca0d3
Alexander Alves <vendas77.rj@gmail.com>
2023-09-18 19:43:41 -0300

    Adicionando Types h

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_05/DXGI_DXUT/Types.h
@@ -0,0 +1,23 @@
+/*=================================================================================
+*  Arquivo     : Types (Arquivo de Cabeçalho)
+*  Author      : Alexander Alves
+*  Criação     : 14 Set 2023
+*  Compilador  : Visual C++ 2019
+*  Atualização : 14 Set 2023
+*
+*  Descrição   : Isola os tipos específicos da plataforma fornecendo 
+*                tipos consistentes para todo o Motor Gráfico.
+===================================================================================*/
+
+#ifndef DXUT_TYPES_H
+#define DXUT_TYPES_H
+
+// atalhos para tipos da linguagem C++
+using uchar  = unsigned char;
+using ushort = unsigned short;
+using ulong  = unsigned long;
+using llong  = long long;
+using ullong = unsigned long long;
+using uint   = unsigned int;
+
+#endif
