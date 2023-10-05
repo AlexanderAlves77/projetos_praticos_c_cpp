7b64cb3b2bdd268cc1ef33849e0809fda1ca8f72
Alexander Alves <vendas77.rj@gmail.com>
2023-10-05 18:19:45 -0300

    Adicionando Mesh h

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_13/Plane/Mesh.h
@@ -0,0 +1,58 @@
+/*=================================================================================
+*  Arquivo     : Mesh (Arquivo de Cabeçalho)
+*  Criação     : 16 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : Visual C++ 2019
+*  Atualização : 16 Set 2023
+*
+*  Descrição   : Representa uma malha 3D em Direct3D 12
+===================================================================================*/
+
+#ifndef DXUT_MESH_H_
+#define DXUT_MESH_H_
+
+// -------------------------------------------------------------------------------
+
+#include <d3d12.h>
+#include "Types.h"
+#include <string>
+using std::string;
+
+// -------------------------------------------------------------------------------
+
+struct Mesh
+{
+    // identificador para recuperar a malha pelo seu nome
+    string id;
+
+    // um Blob é usado para guardar dados de um tipo qualquer
+    // o cliente deve fazer o cast apropriado a partir do Blob
+    
+    // buffers na CPU
+    ID3DBlob* vertexBufferCPU;
+
+    // buffers de Upload CPU -> GPU
+    ID3D12Resource* vertexBufferUpload;
+
+    // buffers na GPU
+    ID3D12Resource* vertexBufferGPU;
+
+    // descritor do vertex buffer
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+
+    // características dos vertex buffers
+    uint vertexByteStride;
+    uint vertexBufferSize;
+
+    // construtor e destrutor
+    Mesh(string name);
+    ~Mesh();
+
+    // retorna descritor (view) do Vertex Buffer
+    D3D12_VERTEX_BUFFER_VIEW * VertexBufferView();
+};
+
+// -------------------------------------------------------------------------------
+
+#endif
+
