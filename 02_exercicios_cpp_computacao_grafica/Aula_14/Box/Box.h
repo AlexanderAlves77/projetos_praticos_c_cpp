b67728715ade707748b5a3ae95943225d2ecebbb
Alexander Alves <vendas77.rj@gmail.com>
2023-10-08 11:11:31 -0300

    Adicionando Box h

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_14/Box/Box.h
@@ -0,0 +1,50 @@
+/*=================================================================================
+*  Arquivo     : Box (Arquivo de Cabeçalho)
+*  Criação     : 16 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : Visual C++ 2019
+*  Atualização : 16 Set 2023
+*
+*  Descrição   : Constrói um cubo 3D com vértices e índices
+===================================================================================*/
+
+#ifndef BOX_H
+#define BOX_H
+
+#include "DXUT.h"
+#include <D3DCompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+using namespace DirectX;
+
+// ------------------------------------------------------------------------------
+
+struct Vertex
+{
+    XMFLOAT3 Pos;
+    XMFLOAT4 Color;
+};
+
+// ------------------------------------------------------------------------------
+
+class Box : public App
+{
+private:
+    ID3D12RootSignature* rootSignature = nullptr;
+    ID3D12PipelineState* pipelineState = nullptr;
+    Mesh* geometry = nullptr;
+
+public:
+    void Init();
+    void Update();
+    void Display();
+    void Finalize();
+
+    void BuildGeometry();
+    void BuildRootSignature();
+    void BuildPipelineState();    
+};
+
+// ------------------------------------------------------------------------------
+
+#endif
