dfea507fae326dbbe2cbf4bf5625b09c9be35638
Alexander Alves <vendas77.rj@gmail.com>
2023-10-11 13:14:38 -0300

    Adicionando Vertex hlsl

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_14/Box/Vertex.hlsl
@@ -0,0 +1,35 @@
+/*=================================================================================
+*  Arquivo     : Vertex (Arquivo de Sombreamento)
+*  Criação     : 16 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : D3DCompiler
+*  Atualização : 15 Set 2023
+*
+*  Descrição   : Um vertex shader simples que apenas passa a posição e cor
+*                do vértice para frente sem realizar nenhuma transformação.
+===================================================================================*/
+
+struct VertexIn
+{
+    float3 PosL  : POSITION;
+    float4 Color : COLOR;
+};
+
+struct VertexOut
+{
+    float4 PosH  : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+VertexOut main(VertexIn vin)
+{
+    VertexOut vout;
+
+    // apenas adiciona a componente w = 1 em cada vértice
+    vout.PosH = float4(vin.PosL, 1.0f);
+
+    // apenas passa a cor do vértice para o pixel shader
+    vout.Color = vin.Color;
+
+    return vout;
+}
