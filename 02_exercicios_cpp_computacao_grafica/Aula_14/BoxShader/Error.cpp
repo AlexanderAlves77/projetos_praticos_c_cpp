5f8e0457c60ba020b1d2880770497f5a7946c6dc
Alexander Alves <vendas77.rj@gmail.com>
2023-10-13 14:46:53 -0300

    Adicionando Error cpp

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_14/BoxShader/Error.cpp
@@ -0,0 +1,56 @@
+/*=================================================================================
+*  Arquivo     : Error (Código Fonte)
+*  Criação     : 16 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : Visual C++ 2019
+*  Atualização : 16 Set 2023
+*
+*  Descrição   : Uma classe para tratamento de erros
+===================================================================================*/
+
+#include "Error.h"              // tratamento de erros
+#include <comdef.h>             // definições COM para C++
+#include <sstream>              // buffer string para texto
+using std::stringstream;        // habilita o uso sem o prefixo std::
+
+// -------------------------------------------------------------------------------
+
+Error::Error() 
+    : hrCode(S_OK), lineNum(-1)
+{
+    // nomes da função e do arquivo vazios
+}
+
+// -------------------------------------------------------------------------------
+
+Error::Error(HRESULT hr, const string & func, const string & file, int line) 
+    : hrCode(hr), funcName(func), lineNum(line)
+{
+    // posição da última barra no nome do arquivo
+    size_t pos = file.find_last_of('\\');
+
+    // se a barra foi encontrada
+    if (pos != string::npos)
+    {
+        // guarda apenas nome do arquivo e não o caminho completo
+        fileName = file.substr(pos + 1);
+    }
+}
+
+// -------------------------------------------------------------------------------
+
+string Error::ToString() const
+{
+    // pega a descrição do código de erro COM em formato string
+    _com_error err(hrCode);
+    
+    stringstream text;
+    text << funcName 
+         << " falhou em " << fileName 
+         << ", linha " << std::to_string(lineNum)
+         << ":\n" << err.ErrorMessage();
+
+    return text.str();
+}
+
+// -------------------------------------------------------------------------------
