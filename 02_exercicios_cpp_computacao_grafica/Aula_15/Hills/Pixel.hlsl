95e1d152da06673d95f06623ea6d6aa020ac7734
Alexander Alves <vendas77.rj@gmail.com>
2023-10-23 19:33:25 -0300

    Adicionando Pixel hlsl

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_15/Hills/Pixel.hlsl
@@ -0,0 +1,21 @@
+/*=================================================================================
+*  Arquivo     : Pixel (Arquivo de Sombreamento)
+*  Criação     : 17 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : D3DCompiler
+*  Atualização : 15 Set 2023
+*
+*  Descrição   : Um pixel shader simples que apenas passa a cor do pixel
+*                para frente sem realizar nenhuma modificação.
+===================================================================================*/
+
+struct pixelIn
+{
+    float4 PosH  : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+float4 main(pixelIn pIn) : SV_TARGET
+{
+    return pIn.Color;
+}
