2d2c2ea9e46007f606387c6cb69aed97f53c6498
Alexander Alves <vendas77.rj@gmail.com>
2023-10-24 16:05:34 -0300

    Adicionando Types h

---
+++ 02_exercicios_cpp_computacao_grafica/Aula_15/Hills/Types.h
@@ -0,0 +1,35 @@
+/*=================================================================================
+*  Arquivo     : Types (Arquivo de Cabeçalho)
+*  Criação     : 17 Set 2023
+*  Author      : Alexander Alves
+*  Compilador  : Visual C++ 2019
+*  Atualização : 17 Set 2023
+*
+*  Descrição   : Isola os tipos específicos da plataforma fornecendo 
+*                tipos consistentes para todo o Motor Gráfico.
+===================================================================================*/
+
+#ifndef DXUT_TYPES_H
+#define DXUT_TYPES_H
+
+// atalhos para tipos da linguagem C++
+using byte   = unsigned char;
+using ushort = unsigned short;
+using ulong  = unsigned long;
+using llong  = long long;
+using ullong = unsigned long long;
+using uint   = unsigned int;
+
+// formatos geométricos
+enum GeometryTypes
+{
+    UNKNOWN_T,
+    BOX_T,
+    CYLINDER_T,
+    SPHERE_T,
+    GEOSPHERE_T,
+    GRID_T,
+    QUAD_T
+};
+
+#endif
