69f4f6a42af7260a1ed2254d3f4d409f7e01868f
Alexander Alves <vendas77.rj@gmail.com>
2023-10-31 14:02:50 -0300

    Função Exibir

--- 04_exercicios_cpp_poo/Aula_01/Estruturado.cpp
+++ 04_exercicios_cpp_poo/Aula_01/Estruturado.cpp
@@ -15,3 +15,36 @@ void atualizar(Jogo& jogo, float valor);
 void jogar(Jogo& jogo, int tempo);
 void exibir(const Jogo& jogo);
 
+Jogo adquirir(const string& titulo, float valor)
+{
+	Jogo novo;
+	novo.nome = titulo;
+	novo.preco = valor;
+	novo.horas = 0;
+	novo.custo = valor;
+	return novo;
+}
+
+void atualizar(Jogo& jogo, float valor)
+{
+	jogo.preco = valor;
+
+	if (jogo.horas > 0)
+		jogo.custo = jogo.preco / jogo.horas;
+}
+
+void jogar(Jogo& jogo, int tempo)
+{
+	jogo.horas += tempo;
+
+	if (jogo.horas > 0)
+		jogo.custo = jogo.preco / jogo.horas;
+}
+
+void exibir(const Jogo& jogo)
+{
+	cout << jogo.nome << " R$"
+		 << jogo.preco << " "
+		 << jogo.horas << "h = R$"
+		 << jogo.custo << "/h\n";
+}
