49bbbf2368d9cb1930d8390fab62cc3e06e4994a
Alexander Alves <vendas77.rj@gmail.com>
2023-11-05 12:12:08 -0300

    Função Translate

--- 04_exercicios_cpp_poo/Aula_05/Point/Point.cpp
+++ 04_exercicios_cpp_poo/Aula_05/Point/Point.cpp
@@ -10,3 +10,12 @@ void Point::MoveTo(int px, int py)
 	x = px;
 	y = py;
 }
+
+
+void Point::Translate(int dx, int dy)
+{
+	x += dx;
+	y += dy;
+}
+
+// -----------------------------------------------
