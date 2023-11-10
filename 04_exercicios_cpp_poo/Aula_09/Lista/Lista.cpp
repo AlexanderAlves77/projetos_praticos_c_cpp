d214bf38a222c9504d9b49f49eb814a9e6443a23
Alexander Alves <vendas77.rj@gmail.com>
2023-11-10 18:55:51 -0300

    Iniciando Lista cpp

---
+++ 04_exercicios_cpp_poo/Aula_09/Lista/Lista.cpp
@@ -0,0 +1,15 @@
+#include "Lista.h"
+#include <iostream>
+using std::cout;
+
+Lista::Lista(int tam)
+{
+	itens = new Item[tam];
+	size = 0;
+	max = tam;
+}
+
+Lista::~Lista()
+{
+	delete[] itens;
+}
