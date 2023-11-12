89bd6db3a92dcadb12efdae4650bb71daa32903e
Alexander Alves <vendas77.rj@gmail.com>
2023-11-12 16:00:28 -0300

    Função Operator Add

--- 04_exercicios_cpp_poo/Aula_09/Tempo/Tempo.cpp
+++ 04_exercicios_cpp_poo/Aula_09/Tempo/Tempo.cpp
@@ -20,3 +20,15 @@ void Tempo::Exibir() const
     cout << horas << " horas, "
          << minutos << " minutos";
 }
+
+Tempo Tempo::operator+(const Tempo& t) const
+{
+    Tempo soma;
+    soma.minutos = minutos + t.minutos;
+    soma.horas = horas + t.horas;
+    
+    // minutos excedentes viram horas
+    soma.horas += soma.minutos / 60;
+    soma.minutos %= 60;
+    return soma;
+}
