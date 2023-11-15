06727ce6d8c5c794d98a3e111b1ec6a32b952f51
Alexander Alves <vendas77.rj@gmail.com>
2023-11-15 16:30:14 -0300

    Teste de Condicional IF

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_06/TesteCondicionalIF/TesteCondicionalIF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int dinheiro = 20;
+
+	if (dinheiro > 10)
+	{
+		cout << "Vou ao Cinema!" << endl;
+	}
+	else
+	{
+		cout << "Vou ficar em casa." << endl;
+	}
+	
+	system("Pause");
+	return 0;
+}
