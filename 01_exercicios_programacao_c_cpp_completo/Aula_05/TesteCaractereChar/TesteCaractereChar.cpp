d04f6c88d5da4e5ee46a8a754e35b98ef5f612f6
Alexander Alves <vendas77.rj@gmail.com>
2023-11-15 16:29:31 -0300

    Teste de Caracteres

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_05/TesteCaractereChar/TesteCaractereChar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	char c = 'a';
+	cout << "Codigo de " << c << " na tabela ASC e: " << int(c) << endl;
+	system("Pause");
+	return 0;
+}
