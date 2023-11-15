570ce45e71055f5875595def96983fa7deac0796
Alexander Alves <vendas77.rj@gmail.com>
2023-11-15 16:28:24 -0300

    Teste de Operadores

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_04/TesteOperadores/TesteOperadores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n1{ 10 }, n2{ 20 };
+	float resultado;
+	resultado = (float)n1 + (float)n2;
+	//resultado = n1 * n2;
+	//resultado = n1 / n2;
+	//resultado = n1 - n2;
+	//resultado = n2 % n1;
+	cout << resultado << endl;
+	system("Pause");
+	return 0;
+}
