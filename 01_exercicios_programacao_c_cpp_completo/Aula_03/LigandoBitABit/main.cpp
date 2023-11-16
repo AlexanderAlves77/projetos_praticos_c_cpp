592452afc19dad847ce351d6b2ce369f36b7ef2f
Alexander Alves <vendas77.rj@gmail.com>
2023-11-16 15:30:56 -0300

    Ligando um Bit

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_03/LigandoBitABit/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Ligar qual bit? (Entre 0 e 7): ";
+	int bit;
+	std::cin >> bit;
+	unsigned char mascara = 1 << bit;
+
+	unsigned char estado = 167;
+	estado = estado | mascara; 
+	std::cout << int(estado) << std::endl;
+
+	return 0;
+}
