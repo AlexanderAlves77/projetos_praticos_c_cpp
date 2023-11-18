86b47f576273c2e87bd9c6a42ae9911c3660b00b
Alexander Alves <vendas77.rj@gmail.com>
2023-11-18 12:43:32 -0300

    RandMod

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_04/RandMod/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Entre com os valores min e max:\n";
+	int min, max;	
+	cin >> min;
+	cin >> max;
+
+	cout << "Sorteando um valor nesta faixa:\n";
+	int sorteio = min + rand() % (max - min + 1);
+	cout << sorteio << endl;
+
+	return 0;
+}
