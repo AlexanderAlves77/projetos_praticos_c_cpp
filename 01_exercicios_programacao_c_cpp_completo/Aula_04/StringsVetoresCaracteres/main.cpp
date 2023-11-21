96919133dcf7b4e6351e5d6d4e321ea5a59f7d0b
Alexander Alves <vendas77.rj@gmail.com>
2023-11-21 19:16:30 -0300

    Strings Vetores e Caracteres

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_04/StringsVetoresCaracteres/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namesapece std;
+
+int main(int argc, char * argv[])
+{
+	char cat[5] = {'m','i','a','r','\0'};
+	char dog[5] = {'l','a','t','i','r'};
+	
+	cout << cat << endl;
+	cout << dog << endl;
+	
+	return 0;
+}
