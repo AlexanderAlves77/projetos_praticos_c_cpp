561dd8d051c5677ade288e95352ecf74f55065dd
Alexander Alves <vendas77.rj@gmail.com>
2023-11-21 19:18:04 -0300

    Teste For

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_04/TesteFor/TesteFor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int num, fat = 1;
+	cout << "Digite um numero: ";
+	cin >> num;
+
+	for (int i = 1; i < num; i++)
+	{
+		fat = fat * (i + 1);		
+	}
+	cout << "Fatorial de: " << num << " é: " << fat << endl;
+	
+	system("Pause");
+	return 0;
+}
