fe23ba657de73a413dd2a74875821d4325a3767a
Alexander Alves <vendas77.rj@gmail.com>
2023-11-30 14:27:12 -0300

    Adicionando Repetir

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_08/Repetir/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int i;   // cria um contador
+
+	// inicializa; testa; atualiza;
+	for (i = 0; i < 5; i++)
+		cout << "C++ sabe repetir." << endl;
+	cout << "C++ sabe quando parar." << endl;
+
+	return 0;
+}
