e0a0e93382b172c8102d20bf703a65d34fbe200e
Alexander Alves <vendas77.rj@gmail.com>
2023-12-04 18:32:42 -0300

    Adicionando Soma Nums

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_10/SomaNums/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Entre com números para somar (0 para sair):\n";
+	int num;
+	int soma = 0;
+	cin >> num;
+
+	while (num != 0)
+	{
+		soma += num;   // soma = soma + num;
+		cin >> num;    // lê o próximo número
+	}
+
+	cout << "A soma é " << soma << "\n";
+}
