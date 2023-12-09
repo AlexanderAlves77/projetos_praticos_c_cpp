c73cfb911a994a76e608396e6548856554bc05da
Alexander Alves <vendas77.rj@gmail.com>
2023-12-09 12:31:47 -0300

    Adicionando UsarLista.cpp

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/Lista/UsarLista.cpp
@@ -0,0 +1,16 @@
+#include "Lista.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Lista lista{ 5 };
+
+	lista.Adicionar(2);
+	lista.Adicionar(3);
+	lista.Adicionar(6);
+
+	for (int i = 0; i < 5; ++i)	{
+		cout << lista[i] << endl;
+	}
+}
