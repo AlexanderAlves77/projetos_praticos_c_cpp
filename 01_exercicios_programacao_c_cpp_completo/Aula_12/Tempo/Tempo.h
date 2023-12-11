9f69ed5a95f67a4be0a5309af6546e0285bbd6e2
Alexander Alves <vendas77.rj@gmail.com>
2023-12-11 19:20:49 -0300

    Adicionando Tempo.h

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/Tempo/Tempo.h
@@ -0,0 +1,15 @@
+class Tempo
+{
+private:
+	int horas;									// quantidade de horas
+	int minutos;								// quantidade de minutos
+
+public:
+	Tempo();									// construtor padrão
+	Tempo(int h, int m = 0);					// construtor com horas e minutos
+	void Exibir() const;						// exibe tempo na tela
+
+	Tempo operator+(const Tempo& t) const;		// retorna soma dos tempos
+	const Tempo& operator+=(const Tempo& t);   // soma de tempo - b = (t += a);
+	const Tempo & operator-=(const Tempo& t);  // subtração de tempo - b = (t += a);
+};
