7cde6f2e5fb97b8aabd371d5073796d28ba437d9
Alexander Alves <vendas77.rj@gmail.com>
2023-12-12 11:17:54 -0300

    Operador Ternário

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/OperadorTernario/OperadorTernario.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/*
+Operador ternário ?:
+<condicao> ? <operacao 1> : <operacao 2>;
+*/
+
+int main(int argc, char* argv[])
+{
+	int num = 10;
+
+	/*
+	if(num > 10)
+		cout << "Numero maior que 10" << endl;
+	else
+		cout << "Numero menor ou igual a 10" << endl;
+	*/
+
+	num > 10 ? cout << "maior" : cout << "menor ou igual";
+	return 0;
+}
