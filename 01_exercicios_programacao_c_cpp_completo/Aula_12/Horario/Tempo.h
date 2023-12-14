ed4a18af079bab9e0b760d381e577bc9270a6ee4
Alexander Alves <vendas77.rj@gmail.com>
2023-12-14 12:30:01 -0300

    Adicionando Tempo.h

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/Horario/Tempo.h
@@ -0,0 +1,29 @@
+// Classe Tempo
+
+#ifndef _TEMPO_H_
+#define _TEMPO_H_
+#include <iostream>
+using namespace std;
+
+class Tempo
+{
+private:
+	int horas;
+	int minutos;
+
+public:
+	Tempo(int h = 0, int m = 0);
+	~Tempo();
+	/*
+	operator double();
+	operator int();
+	*/
+	Tempo operator+(const Tempo& t) const;
+	friend Tempo operator+(const Tempo& t1, const Tempo& t2);
+	friend Tempo operator+(const Tempo& t, int h);
+	friend Tempo operator+(int h, const Tempo& t);
+	friend ostream& operator<<(ostream& os, const Tempo& t);
+};
+
+#endif
+
