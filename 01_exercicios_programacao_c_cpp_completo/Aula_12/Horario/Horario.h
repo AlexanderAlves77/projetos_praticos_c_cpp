ee8de4a47b1ac697051afe8a1813d0bdc65858ce
Alexander Alves <vendas77.rj@gmail.com>
2023-12-14 12:30:32 -0300

    Adicionando Horario.h

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/Horario/Horario.h
@@ -0,0 +1,22 @@
+// Classe Horario
+
+#ifndef _HORARIO_H_
+#define _HORARIO_H_
+#include "Tempo.h"
+#include <iostream>
+using namespace std;
+
+class Horario
+{
+private:
+	int horas;
+	int minutos;
+
+public:
+	Horario(int h = 0, int m = 0);
+
+	friend Horario operator-(const Horario& h1, const Horario& h2);
+	friend ostream& operator<<(ostream& os, const Horario& h);
+};
+
+#endif
