8c7f47349adf0d0eda5052225fa3796bead5b59e
Alexander Alves <vendas77.rj@gmail.com>
2023-12-15 19:23:34 -0300

    Funções IO

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/FluxoIO/FluxoIO.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int num;
+	cout << "Digite um numero: ";
+	cin >> num;
+	cout << "Você digitou: " << num << endl;
+	return 0;
+}
