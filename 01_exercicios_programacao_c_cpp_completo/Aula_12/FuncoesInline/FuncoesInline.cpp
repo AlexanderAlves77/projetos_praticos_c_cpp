5ff595ed960730da461cbc21db85fabfb54b01ee
Alexander Alves <vendas77.rj@gmail.com>
2023-12-15 19:22:51 -0300

    Funções Inline

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_12/FuncoesInline/FuncoesInline.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+inline void foo()
+{
+	cout << "Apredendo C++" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	foo();
+	return 0;
+}
