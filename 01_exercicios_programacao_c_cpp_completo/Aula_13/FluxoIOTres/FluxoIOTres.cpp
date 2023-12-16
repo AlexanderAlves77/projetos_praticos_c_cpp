0f684b8306cbbdc9980537ec022e5c5992f0a93b
Alexander Alves <vendas77.rj@gmail.com>
2023-12-16 13:06:12 -0300

    Fluxo IO Tres

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_13/FluxoIOTres/FluxoIOTres.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ofstream output("saida.txt");
+	output << "Aprendendo C++\n";
+	output << "https://www.fulldevstacks.com.br\n";
+	
+	return 0;
+}
+
+
+
+
+
+
