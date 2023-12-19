1177f2c49c537f094c19d061067fe9d932ad36fa
Alexander Alves <vendas77.rj@gmail.com>
2023-12-19 11:33:47 -0300

    Criando Namespace

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_14/CriandoNamespace/CriandoNamespace.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+namespace ns1
+{
+	int num = 42;
+}
+
+namespace ns2
+{
+	int num = 50;
+}
+
+//using namespace ns1;
+//using namespace ns2;
+
+int main(int argc, char* argv[])
+{
+	std::cout << ns1::num << std::endl;
+	std::cout << ns2::num << std::endl;
+	return 0;
+}
