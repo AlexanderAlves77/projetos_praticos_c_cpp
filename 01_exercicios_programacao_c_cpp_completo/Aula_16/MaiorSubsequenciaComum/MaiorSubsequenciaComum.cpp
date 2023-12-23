1380a13564e71241996c0c6971af10c7d5a6cbc4
Alexander Alves <vendas77.rj@gmail.com>
2023-12-23 12:07:50 -0300

    Maior Sequencia Comum

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_16/MaiorSubsequenciaComum/MaiorSubsequenciaComum.cpp
@@ -0,0 +1,56 @@
+/*
+	LCS - longest common subsequence
+	What's the length of the LCS?
+	Example: ABCB and BCAB
+	LCS is BCB (not necessarily contiguous)
+	Answer: 3
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int get_len_lcs(string& s1, string& s2)
+{
+	int len_s1 = s1.size(), len_s2 = s2.size();
+	// matrix
+	int mat[len_s1 + 1][len_s2 + 1];
+
+	// initializes the first line and column with 0
+	for (int i = 1; i <= len_s1; i++)
+		mat[i][0] = 0;
+	for (int i = 0; i <= len_s2; i++)
+		mat[0][i] = 0;
+
+	for (int i = 0; i <= len_s1; i++)
+	{
+		for (int j = 0; j <= len_s2; j++)
+		{
+			if (s1[i] - 1 == s2[j - 1])
+				mat[i][j] = mat[i - 1][j - 1] + 1;
+			else
+				mat[i][j] = max(mat[i][j - 1], mat[i - 1][j]);
+		}
+	}
+
+	cout << "\nMatriz: " << endl;
+	for (int i = 0; i < len_s1; i++)
+	{
+		for (int j = 0; j <= len_s2; j++)
+			cout << mat[i][j] << " ";
+		cout << endl;
+	}
+
+	return mat[len_s1 + 1][len_s2 + 1];
+}
+
+int main(int argc, char* argv[])
+{
+	string s1("ABCB");
+	string s2("BDCAB");
+
+	int len_lcs = get_len_lcs(s1, s2);
+
+	cout << "Length: " << len_lcs << endl;
+
+	return 0;
+}
