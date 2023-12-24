cfe63824b90d35e032a7480f84a6b8590655b5c2
Alexander Alves <vendas77.rj@gmail.com>
2023-12-24 16:22:04 -0300

    Machine Learning KNN

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_16/MachineLearningKNN/MachineLearningKNN.cpp
@@ -0,0 +1,13 @@
+// Implementação do k-NN (k-nearest neighbors algorithm)
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <map>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
