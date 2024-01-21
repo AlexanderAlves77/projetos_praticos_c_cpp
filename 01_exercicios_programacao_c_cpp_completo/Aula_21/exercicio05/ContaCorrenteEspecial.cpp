dc144440a2740ef27d59e19e49218eb851dd8465
Alexander Alves <vendas77.rj@gmail.com>
2024-01-21 13:29:39 -0300

    Conta Corrente Especial.cpp

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_21/exercicio05/ContaCorrenteEspecial.cpp
@@ -0,0 +1,18 @@
+/*
+ * ContaCorrenteEspecial.cpp
+ *
+ * Implementação de ContaCorrenteEspecial.
+ *
+ * Parte do exercício 4.
+ *
+ *  Created on: Aug 12, 2013
+ *      Author: Vítor E. Silva Souza (vitorsouza@gmail.com)
+ */
+
+#include "ContaCorrenteEspecial.h"
+
+namespace exercicio05 {
+	double ContaCorrenteEspecial::getTaxaOperacao() {
+		return 0.01;
+	}
+}
