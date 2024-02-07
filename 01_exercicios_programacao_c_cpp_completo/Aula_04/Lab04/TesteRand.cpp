fead27bf79ead5fe36027c62ffaf5859f5d09f5d
Alexander Alves <vendas77.rj@gmail.com>
2024-02-07 14:41:40 -0300

    Adicionando TesteRand.cpp

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_04/Lab04/TesteRand.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	srand(1);
+	cout << rand() << " ";
+	cout << rand() << " ";
+	cout << rand() << " ";
+	cout << rand() << " ";
+	
+
+	return 0;
+}
+
