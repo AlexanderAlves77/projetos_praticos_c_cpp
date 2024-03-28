e73aed831cdb285f0f87f90277e919d6e3565a50
Alexander Alves <vendas77.rj@gmail.com>
2024-03-28 17:21:53 -0300

    Adicionando DXUT.h

---
+++ 01_exercicios_programacao_c_cpp_completo/Aula_01/Jogos_Lab_01/WinDXUT/DXUT.h
@@ -0,0 +1,20 @@
+/**********************************************************************************
+* Arquivo    : DXUT (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 04 Jan 2020
+* Atualização: 11 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Arquivo mestre para a biblioteca DirectX Utility (DXUT)
+* 
+**********************************************************************************/
+
+
+#ifndef DXUT_DirectX_Utility_H
+#define DXUT_DirectX_Utility_H
+
+#include "Types.h"
+#include "Resources.h"
+#include "Window.h"
+
+#endif
