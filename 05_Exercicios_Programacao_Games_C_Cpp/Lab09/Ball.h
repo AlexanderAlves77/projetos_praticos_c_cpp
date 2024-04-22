f9bda55afbde575620428343657e14b458608a85
Alexander Alves <vendas77.rj@gmail.com>
2024-04-22 06:59:37 -0300

    Adicionando Ball.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab09/Ball.h
@@ -0,0 +1,42 @@
+/**********************************************************************************
+* Arquivo    : Ball (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 22 Dez 2012
+* Atualização: 13 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Bola do jogo Breakout
+*
+**********************************************************************************/
+
+#ifndef _BREAKOUT_BALL_H_
+#define _BREAKOUT_BALL_H_
+
+// ---------------------------------------------------------------------------------
+// Inclusões
+
+#include "Types.h"            // tipos específicos da engine
+#include "Object.h"            // interface de Object
+#include "Sprite.h"            // interface de Sprites
+#include "Player.h"            // jogador do Breakout
+
+// ---------------------------------------------------------------------------------
+
+class Ball : public Object
+{
+private:
+	Player * player;
+	Sprite * sprite;
+	float velX, velY;
+
+public:
+	Ball(Player * p);
+	~Ball();
+
+	void Update();
+	void Draw();
+};
+
+// ---------------------------------------------------------------------------------
+
+#endif
