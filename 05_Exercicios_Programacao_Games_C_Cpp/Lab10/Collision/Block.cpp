49ccedb1fb8601b3c1eebae49f8afbc50d5f500f
Alexander Alves <vendas77.rj@gmail.com>
2024-05-01 10:33:34 -0300

    Adicionando Block.cpp

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab10/Collision/Block.cpp
@@ -0,0 +1,52 @@
+/**********************************************************************************
+* Arquivo    : Block (Código Fonte)
+* Author     : Alexander Alves
+* Criação    : 22 Dez 2012
+* Atualização: 13 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Bloco do jogo Breakout
+*
+**********************************************************************************/
+
+#include "Block.h"
+#include "Breakout.h"
+
+// ---------------------------------------------------------------------------------
+
+Block::Block(Image * img)
+{
+    // tamanho do bloco é 60x24
+    BBox(new Rect(0, 0, 59, 23));
+
+    // sprite do bloco
+    sprite  = new Sprite(img);
+
+    // velocidades iniciais
+    velX = 0.0f;
+    velY = 0.0f;
+
+    // tipo do objeto
+    type = BLOCK;
+}
+
+// ---------------------------------------------------------------------------------
+
+Block::~Block()
+{
+    delete sprite;
+}
+
+// ---------------------------------------------------------------------------------
+
+void Block::Update()
+{
+    // desloca o bloco
+    Translate(velX * gameTime, velY * gameTime);
+
+    // destrói ao sair da janela
+    if (y > window->Height())
+        Breakout::scene->Delete();
+}
+
+// ---------------------------------------------------------------------------------
