f78c37d8025b4f8c268e8fd21404280c0c9d7b91
Alexander Alves <vendas77.rj@gmail.com>
2024-05-08 08:15:07 -0300

    Adicionando Home.cpp

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab11/PacMan/Home.cpp
@@ -0,0 +1,57 @@
+/**********************************************************************************
+* Arquivo    : Home (Código Fonte)
+* Author     : Alexander Alves
+* Criação    : 18 Jan 2013
+* Atualização: 14 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Tela de abertura do jogo PacMan
+*
+**********************************************************************************/
+
+#include "Engine.h"
+#include "Home.h"
+#include "Level1.h"
+
+// ------------------------------------------------------------------------------
+
+void Home::Init()
+{
+    backg = new Sprite("Resources/TitleScreen.jpg");
+}
+
+// ------------------------------------------------------------------------------
+
+void Home::Finalize()
+{
+    delete backg;
+}
+
+// ------------------------------------------------------------------------------
+
+void Home::Update()
+{
+    // sai do jogo com a tecla ESC
+    if (ctrlKeyESC && window->KeyDown(VK_ESCAPE))
+    {
+        ctrlKeyESC = false;
+        window->Close();
+    }
+    else if (window->KeyUp(VK_ESCAPE))
+    {
+        ctrlKeyESC = true;
+    }
+
+    // passa ao primeiro nível com ENTER
+    if (window->KeyDown(VK_RETURN))
+        Engine::Next<Level1>();
+}
+
+// ------------------------------------------------------------------------------
+
+void Home::Draw()
+{
+    backg->Draw(float(window->CenterX()), float(window->CenterY()), Layer::BACK);
+}
+
+// ------------------------------------------------------------------------------
