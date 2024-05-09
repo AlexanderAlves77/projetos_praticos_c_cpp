92bcc936e255030a6e7bb11bf6cbda4a9af718dd
Alexander Alves <vendas77.rj@gmail.com>
2024-05-09 07:46:41 -0300

    Adicionando Pivot.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab11/PacMan/Pivot.h
@@ -0,0 +1,40 @@
+/**********************************************************************************
+* Arquivo    : Pivot (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 01 Jan 2013
+* Atualização: 14 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Ponto de mudança de direção para o jogador e fantasmas
+*
+**********************************************************************************/
+
+#ifndef _PACMAN_PIVOT_H_
+#define _PACMAN_PIVOT_H_
+
+// ---------------------------------------------------------------------------------
+// Inclusões
+
+#include "Types.h"                          // tipos específicos da engine
+#include "Object.h"                         // interface de Object
+
+// ---------------------------------------------------------------------------------
+
+class Pivot : public Object
+{
+public:
+    bool left  = false;                     // passagem livre a esquerda
+    bool right = false;                     // passagem livre a direita
+    bool up    = false;                     // passagem livre para cima
+    bool down  = false;                     // passagem livre para baixo
+
+    Pivot(bool l, bool r, bool u, bool d);  // construtor
+    ~Pivot();                               // destrutor
+
+    void Update() {};                       // atualização
+    void Draw() {};                         // desenho
+};
+
+// ---------------------------------------------------------------------------------
+
+#endif
