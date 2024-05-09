01f47dfb4a29c058ef44c942f336b7c4e9c3e5e0
Alexander Alves <vendas77.rj@gmail.com>
2024-05-09 07:46:13 -0300

    Adicionando PacMan.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab11/PacMan/PacMan.h
@@ -0,0 +1,30 @@
+/**********************************************************************************
+* Arquivo    : PacMan (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 01 Jan 2013
+* Atualização: 14 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Exercício sobre detecção de colisão
+*
+**********************************************************************************/
+
+
+#ifndef _PACMAN_H_
+#define _PACMAN_H_
+
+// ------------------------------------------------------------------------------
+
+enum PacManObjects
+{
+    PLAYER,
+    GHOST,
+    FOOD,
+    SPECIAL,
+    PIVOT
+};
+
+// ------------------------------------------------------------------------------
+
+#endif
+
