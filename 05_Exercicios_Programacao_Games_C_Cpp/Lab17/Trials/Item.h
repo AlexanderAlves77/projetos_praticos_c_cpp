103be0d46fa46a1722d284331bea18e7c2d02c6c
Alexander Alves <vendas77.rj@gmail.com>
2024-08-11 11:49:44 -0300

    Adicionando Item.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab17/Trials/Item.h
@@ -0,0 +1,47 @@
+/**********************************************************************************
+* Arquivo    : Item (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 14 Mai 2012
+* Atualização: 19 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Define um item de menu
+*
+**********************************************************************************/
+
+#ifndef _TRIALSMENU_ITEM_H_
+#define _TRIALSMENU_ITEM_H_
+
+// ---------------------------------------------------------------------------------
+
+#include "Object.h"
+#include "Animation.h"
+#include "TileSet.h"
+#include "Types.h"
+
+// ---------------------------------------------------------------------------------
+
+enum MenuState {SELECTED, NOTSELECTED};
+
+// ---------------------------------------------------------------------------------
+
+class Item : public Object
+{
+private:
+    TileSet   * tileset;                // folha de sprites da animação
+    Animation * animation;              // animação do menu
+    
+public:
+    Item(float posX, float posY, uint menuId, string imgFile);
+    ~Item();
+
+    void Select();                      // seleciona menu
+    void UnSelect();                    // desmarca seleção
+
+    void Update();                      // atualização do objeto
+    void Draw();                        // desenho do objeto
+}; 
+
+// ---------------------------------------------------------------------------------
+
+#endif
