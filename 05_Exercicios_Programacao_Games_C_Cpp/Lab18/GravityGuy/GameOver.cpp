7b5008194665a420c8bec49f0a3c5f5d2635a6d6
Alexander Alves <vendas77.rj@gmail.com>
2024-08-20 16:03:27 -0300

    Adicionando GameOver.cpp

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab18/GravityGuy/GameOver.cpp
@@ -0,0 +1,47 @@
+/**********************************************************************************
+* Arquivo    : GameOver (Código Fonte)
+* Author     : Alexander Alves
+* Criação    : 14 Fev 2013
+* Atualização: 19 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Fim do jogo
+*
+**********************************************************************************/
+
+#include "Engine.h"
+#include "GameOver.h"
+#include "GravityGuy.h"
+#include "Home.h"
+
+// ----------------------------------------------------------------------
+
+void GameOver::Init()
+{
+    title = new Sprite("Resources/GameOver.png");
+}
+
+// ----------------------------------------------------------------------
+
+void GameOver::Update()
+{
+    if (window->KeyPress(VK_ESCAPE) || window->KeyPress(VK_RETURN))
+        GravityGuy::NextLevel<Home>();
+}
+
+// ----------------------------------------------------------------------
+
+void GameOver::Draw()
+{
+    title->Draw(window->CenterX(), window->CenterY(), Layer::BACK);
+}
+
+// ----------------------------------------------------------------------
+
+void GameOver::Finalize()
+{
+    delete title;
+}
+
+// ----------------------------------------------------------------------
+
