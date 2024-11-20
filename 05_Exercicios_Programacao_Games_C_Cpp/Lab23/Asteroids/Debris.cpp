bd621559a84efa811c2f3bfcf3f5b4f2d2e75065
Alexander Alves <vendas77.rj@gmail.com>
2024-11-20 08:50:37 -0300

    Adicionando Debris.cpp

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab23/Asteroids/Debris.cpp
@@ -0,0 +1,40 @@
+/**********************************************************************************
+* Arquivo    : Debris (Código Fonte)
+* Author     : Alexander Alves
+* Criação    : 07 Fev 2013
+* Atualização: 21 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Animação de explosão
+*
+**********************************************************************************/
+
+#include "Debris.h"
+#include "Asteroids.h"
+
+// ---------------------------------------------------------------------------------
+
+Debris::Debris(float posX, float posY)
+{
+    anim = new Animation(Asteroids::debriSet, 0.060f, false);
+    MoveTo(posX, posY);
+}
+
+// ---------------------------------------------------------------------------------
+
+Debris::~Debris()
+{
+    delete anim;
+}
+
+// ---------------------------------------------------------------------------------
+
+void Debris::Update()
+{
+    anim->NextFrame();
+
+    if (anim->Inactive())
+        Asteroids::scene->Delete();    
+}
+
+// ---------------------------------------------------------------------------------
