b4a7fd6c71d01e09069e71a3e8ac189048e8fb18
Alexander Alves <vendas77.rj@gmail.com>
2024-12-03 07:14:15 -0300

    Adicionando Mouse.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab23/CollisionT/Mouse.h
@@ -0,0 +1,35 @@
+/**********************************************************************************
+* Arquivo    : Mouse (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 27 Jul 2019
+* Atualização: 21 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Gerencia mouse na tela
+*
+**********************************************************************************/
+
+#ifndef _COLLISIONT_MOUSE_H_
+#define _COLLISIONT_MOUSE_H_
+
+// ---------------------------------------------------------------------------------
+
+#include "Object.h"        
+#include "Types.h"        
+
+// ---------------------------------------------------------------------------------
+
+class Mouse : public Object
+{
+public:
+    Mouse();                            // construtor
+    ~Mouse();                           // destrutor
+
+    void OnCollision(Object* obj);      // resolução de colisão
+    void Update();                      // atualização
+    void Draw();                        // desenho
+}; 
+
+// ---------------------------------------------------------------------------------
+
+#endif
