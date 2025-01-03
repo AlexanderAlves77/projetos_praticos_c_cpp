de3a05a1a5010c7f784c97a575e56c7121c5eba1
Alexander Alves <vendas77.rj@gmail.com>
2025-01-03 07:03:52 -0300

    Adicionando Delay.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab25/GeoWars/Delay.h
@@ -0,0 +1,40 @@
+/**********************************************************************************
+* Arquivo    : Delay (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 02 Ago 2019
+* Atualização: 21 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Sincroniza uma ação
+*
+**********************************************************************************/
+
+#ifndef _GEOWARS_DELAY_H_
+#define _GEOWARS_DELAY_H_
+
+// --------------------------------------------------------------------------------
+
+#include "Object.h"
+#include "Sprite.h"
+#include "Timer.h"
+
+// --------------------------------------------------------------------------------
+
+class Delay : public Object
+{
+private:
+    Sprite * logo;          // logotipo do jogo
+    Timer timer;            // medidor de tempo
+    bool notPlayed;         // intro não tocada ainda
+
+public:
+    Delay();                // construtor
+    ~Delay();               // destrutor
+
+    void Update();          // atualização
+    void Draw();            // desenho
+};
+
+// ------------------------------------------------------------------------------
+
+#endif
