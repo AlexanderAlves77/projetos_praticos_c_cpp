d767d4394f55facd51e73f795da6a17217b9990a
Alexander Alves <vendas77.rj@gmail.com>
2025-03-05 10:56:46 -0300

    Allocating a backbuffer

--- 05_Exercicios_Programacao_Games_C_Cpp/Lab30/handmade/handmade/main.cpp
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab30/handmade/handmade/main.cpp
@@ -4,7 +4,7 @@
 * Created     : Jan 01 2022
 * Update      : Jan 01 2022
 * Compiler    : Visual C++ 2022
-* Description : 
+* Description : Introduction to games 
 *
 **********************************************************************************/
 
@@ -12,8 +12,57 @@
 #include <windows.h>
 #include <windowsx.h>
 
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void * BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
 LRESULT CALLBACK WinProc(HWND, UINT, WPARAM, LPARAM);
+
+void Win32ResizeDIBSection(int Width, int Height)
+{
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BitmapInfo.bmiHeader.biSizeImage = 0;
+    BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biClrUsed = 0;
+    BitmapInfo.bmiHeader.biClrImportant = 0;
+
+    BitmapHandle =  CreateDIBSection(
+        BitmapDeviceContext,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0, 0);
+}
+
+void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(
+        DeviceContext,
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY);
+}
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstPrev, _In_ LPSTR cmdline, _In_ int cmdShow)
 {
@@ -44,16 +93,16 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstPrev, _In
     WindowHandle = CreateWindowEx(
         0,
         WindowClass.lpszClassName,
-        "Handemade Hero",               
-        WS_OVERLAPPEDWINDOW|WS_VISIBLE,        
-        CW_USEDEFAULT,              
-        CW_USEDEFAULT,              
-        CW_USEDEFAULT,              
-        CW_USEDEFAULT,              
-        NULL,                       
-        NULL,                       
-        hInstance,                  
-        NULL);                      
+        "Handemade Hero",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
 
     // Show and update the window
     ShowWindow(WindowHandle, cmdShow);
@@ -66,7 +115,6 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstPrev, _In
         DispatchMessage(&msg);
     }
 
-
     return int(msg.wParam);
 }
 
@@ -77,29 +125,37 @@ LRESULT CALLBACK WinProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam
     {
     case WM_SIZE:
     {
-        OutputDebugStringA("WM_SIZE\n");
-    } break;
-    case WM_DESTROY:
-    {
-        OutputDebugStringA("WM_DESTROY\n");
-    } break;
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        int Width = ClientRect.right - ClientRect.left;
+        int Height = ClientRect.bottom - ClientRect.top;
+        Win32ResizeDIBSection(Width, Height);
+    } break;    
     case WM_CLOSE:
     {
+        Running = false;
+        PostQuitMessage(0);
         OutputDebugStringA("WM_CLOSE\n");
     } break;
     case WM_ACTIVATEAPP:
     {
         OutputDebugStringA("WM_ACTIVATEAPP\n");
     } break;
+    case WM_DESTROY:
+    {
+        Running = false;
+        OutputDebugStringA("WM_DESTROY\n");
+    } break;
     case WM_PAINT:
     {
         PAINTSTRUCT Paint;
-        HDC DeviceContext = BeginPaint( Window, & Paint );
+        HDC DeviceContext = BeginPaint(Window, &Paint);
         int X = Paint.rcPaint.left;
         int Y = Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        static DWORD Operation = WHITENESS;
+        Win32UpdateWindow(Window, X, Y, Width, Height);
+        local_persist DWORD Operation = WHITENESS;
         PatBlt(DeviceContext, X, Y, Width, Height, Operation);
         if (Operation == WHITENESS)
         {
