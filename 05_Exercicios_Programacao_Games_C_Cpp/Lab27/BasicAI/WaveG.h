f93111b07c71a6f286e65aa300a8d93fdbb71bc4
Alexander Alves <vendas77.rj@gmail.com>
2025-04-01 07:35:06 -0300

    Adicionando WaveG.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab27/BasicAI/WaveG.h
@@ -0,0 +1,41 @@
+/**********************************************************************************
+* Arquivo    : WaveG (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 06 Ago 2019
+* Atualização: 23 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Cria uma onda de inimigos Green
+*
+**********************************************************************************/
+
+
+#ifndef _BASICAI_WAVEG_H_
+#define _BASICAI_WAVEG_H_
+
+// --------------------------------------------------------------------------------
+
+#include "Object.h"
+#include "Timer.h"
+#include "Random.h"
+
+// --------------------------------------------------------------------------------
+
+class WaveG : public Object
+{
+private: 
+    RandF secs;             // valor aleatório de segundos
+    Timer timer;            // medidor de tempo
+    float delay;            // atraso para a próxima onda
+
+public:
+    WaveG();                // construtor
+    ~WaveG();               // destrutor
+
+    void Update();          // atualização
+    void Draw();            // desenho
+};
+
+// ------------------------------------------------------------------------------
+
+#endif
