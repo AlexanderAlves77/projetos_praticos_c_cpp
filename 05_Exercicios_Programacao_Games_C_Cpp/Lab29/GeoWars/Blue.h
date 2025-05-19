06ad1f6a10eff43a79c2800773a67b96a38bcdf3
Alexander Alves <vendas77.rj@gmail.com>
2025-05-19 15:59:37 -0300

    Adicionando Blue.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab29/GeoWars/Blue.h
@@ -0,0 +1,50 @@
+/**********************************************************************************
+* Arquivo    : Blue (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 10 Out 2012
+* Atualização: 21 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Objeto faz uma perseguição suavizada
+*
+**********************************************************************************/
+
+#ifndef _GEOWARS_BLUE_H_
+#define _GEOWARS_BLUE_H_
+
+// ---------------------------------------------------------------------------------
+
+#include "Object.h"    
+#include "Types.h"
+#include "Sprite.h"
+#include "Vector.h"
+#include "Player.h"
+
+// ---------------------------------------------------------------------------------
+
+class Blue : public Object
+{
+private:
+    Sprite * sprite;                            // sprite do objeto
+    Player * player;                            // ponteiro para jogador
+    Vector speed;                               // velocidade e direção
+    float factor;                               // fator de escala
+    
+public:
+    Blue(float pX, float pY, Player * p);       // construtor
+    ~Blue();                                    // destrutor
+    
+    void OnCollision(Object* obj);              // resolução da colisão
+    void Update();                              // atualização
+    void Draw();                                // desenho
+}; 
+
+// ---------------------------------------------------------------------------------
+
+inline void Blue::Draw()
+{ sprite->Draw(x, y, Layer::LOWER, scale, rotation); }
+
+// ---------------------------------------------------------------------------------
+
+
+#endif
