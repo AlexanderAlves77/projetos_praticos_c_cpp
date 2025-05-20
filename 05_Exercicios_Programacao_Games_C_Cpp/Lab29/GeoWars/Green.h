d7bec6edd1a7bc776b386ab04deb4d7ebe3c9f1b
Alexander Alves <vendas77.rj@gmail.com>
2025-05-20 09:27:48 -0300

    Adicionando Green.h

---
+++ 05_Exercicios_Programacao_Games_C_Cpp/Lab29/GeoWars/Green.h
@@ -0,0 +1,50 @@
+/**********************************************************************************
+* Arquivo    : Green (Arquivo de Cabeçalho)
+* Author     : Alexander Alves
+* Criação    : 15 Out 2012
+* Atualização: 21 Dez 2023
+* Compilador : Visual C++ 2019
+*
+* Descrição  : Objeto faz uma fuga suavizada
+*
+**********************************************************************************/
+
+#ifndef _GEOWARS_GREEN_H_
+#define _GEOWARS_GREEN_H_
+
+// ---------------------------------------------------------------------------------
+
+#include "Object.h"    
+#include "Types.h"
+#include "Sprite.h"
+#include "Vector.h"
+#include "Player.h"
+
+// ---------------------------------------------------------------------------------
+
+class Green : public Object
+{
+private:
+    Sprite * sprite;                            // sprite do objeto
+    Player * player;                            // ponteiro para jogador
+    Vector speed;                               // velocidade e direção
+    int distance;                               // distância do jogador
+    
+public:
+    Green(float pX, float pY, Player * p);      // construtor
+    ~Green();                                   // destrutor
+    
+    void OnCollision(Object* obj);              // resolução da colisão
+    void Update();                              // atualização
+    void Draw();                                // desenho
+}; 
+
+// ---------------------------------------------------------------------------------
+
+inline void Green::Draw()
+{ sprite->Draw(x, y, Layer::LOWER, scale, rotation); }
+
+// ---------------------------------------------------------------------------------
+
+
+#endif
