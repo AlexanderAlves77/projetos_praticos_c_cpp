11caa8542914a9215a626314c942453e8febd02a
Alexander Alves <vendas77.rj@gmail.com>
2026-08-30 13:24:32 -0300

    feat: add first C++ program

---
+++ 07_Game_Engineering_na_Pratica_CPP_SFML/CppGameEngine/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Game Engineering com C++ começou" << std::endl;
+	std::cout << "Meu primeiro projeto está funcionando." << std::endl;
+	std::cout << "Um dia isso será um jogo." << std::endl;
+	return 0;
+}
