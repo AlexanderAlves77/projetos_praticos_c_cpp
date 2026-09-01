7462bd9e57cba3db9a9f30833ccadc0363835aeb
Alexander Alves <vendas77.rj@gmail.com>
2026-09-01 16:20:04 -0300

    feat: add counted repetition with for loop

--- 07_Game_Engineering_na_Pratica_CPP_SFML/CppGameEngine/main.cpp
+++ 07_Game_Engineering_na_Pratica_CPP_SFML/CppGameEngine/main.cpp
@@ -2,24 +2,31 @@
 
 int main()
 {
-    int day = 1;
     const int TOTAL_DAYS = 5;
+    const int RESOURCES_PER_DAY = 3;
+
+    int totalResources = 0;
 
     std::cout << "=== SIMULATION START ===" << std::endl;
 
-    while (day <= TOTAL_DAYS)
+    for (int day = 1; day <= TOTAL_DAYS; day++)
     {
+        totalResources += RESOURCES_PER_DAY;
+
         std::cout << "Day " << day << std::endl;
+        std::cout << "Resources Produced Today: " << RESOURCES_PER_DAY << std::endl;
+        std::cout << "Total Resources: " << totalResources << std::endl;
 
         if (day == 3)
         {
             std::cout << "A special event happened." << std::endl;
         }
 
-        day++;
+        std::cout << std::endl;
     }
 
     std::cout << "Simulation finished." << std::endl;
+    std::cout << "Final Resources: " << totalResources << std::endl;
 
 	return 0;
 }
